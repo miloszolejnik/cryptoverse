c86f2df0a84e52c26fbaff196b4b31abe9415859
Milosz Olejnik <milosz.olejnik@tuta.io>
2021-12-13 20:59:49 +0100

    create news api and news card component

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,1 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'e5de9364aamsheccb6f40d792c34p1db238jsn7d6108a2c855'
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+})
+
+export const {
+    useGetCryptoNewsQuery,
+} = cryptoNewsApi;
